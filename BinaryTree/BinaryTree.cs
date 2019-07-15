d3e26dbac703dbe7afe0c6e48b5defcadd20e6ea
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-07-16 02:35:05 +0300

    BinaryTree- Remove Method partially implemented

--- BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree.cs
@@ -49,14 +49,14 @@ namespace BinaryTree
             }
         }
 
-        public void RemoveChild(Node<T> node)
-        {
-            throw new NotImplementedException();
-        }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public IEnumerable<T> InOrderTraversal()
+        {
+            return InOrderTraversal(root);
         }
 
         public void InsertChild(Node<T> node)
@@ -77,9 +77,9 @@ namespace BinaryTree
             }
         }
 
-        public bool Remove(T item)
+        public IEnumerable<T> PostOrderTraversal()
         {
-            throw new NotImplementedException();
+            return PostOrderTraversal(root);
         }
 
         public IEnumerable<T> PreOrderTraversal()
@@ -87,60 +87,78 @@ namespace BinaryTree
             return PreOrderTraversal(root);
         }
 
-        public IEnumerable<T> PostOrderTraversal()
+        public bool Remove(T item)
         {
-            return PostOrderTraversal(root);
+            Node<T> parent = new Node<T>(root.Data);
+            var foundNode = FindNode(root, item, ref parent);
+            if (foundNode == null)
+            {
+                return false;
+            }
+
+            if (IsLeaf(foundNode))
+            {
+                RemoveLeafNode(parent, foundNode);
+            }
+            else if (CheckForOneChild(parent))
+            {
+                RemoveOnlyChild(parent, foundNode);
+            }
+
+            Count--;
+            return true;
         }
 
-        public IEnumerable<T> InOrderTraversal()
-        {
-            return InOrderTraversal(root);
-        }
-
-        private IEnumerable<T> PreOrderTraversal(Node<T> node)
+        public void RemoveChild(Node<T> node)
         {
             if (node == null)
             {
-                yield break;
+                return;
             }
 
-            yield return node.Data;
-
-            foreach (T leftNode in PreOrderTraversal(node.Left))
-            {
-                yield return leftNode;
-            }
-
-            foreach (T rightNode in PreOrderTraversal(node.Right))
-            {
-                yield return rightNode;
-            }
+            Remove(node.Data);
         }
 
-        private IEnumerable<T> PostOrderTraversal(Node<T> node)
+        private bool CheckForOneChild(Node<T> parent)
         {
-            if (node == null)
+            return (parent.Right == null && parent.Left != null)
+                || (parent.Left == null && parent.Right != null);
+        }
+
+        private Node<T> FindNode(Node<T> rootNode, T item)
+        {
+            var newNode = new Node<T>(root.Data);
+            return FindNode(rootNode, item, ref newNode);
+        }
+
+        private Node<T> FindNode(Node<T> rootNode, T item, ref Node<T> parent)
+        {
+            if (rootNode == null)
             {
-                yield break;
+                return null;
             }
 
-            if (node.Left != null)
+            if (Count == 1)
             {
-                foreach (var leftNode in PostOrderTraversal(node.Left))
-                {
-                    yield return leftNode;
-                }
+                parent = rootNode;
             }
 
-            if (node.Right != null)
+            if (rootNode.Left?.Data.Equals(item) == true || rootNode.Right?.Data.Equals(item) == true)
             {
-                foreach (var rightNode in PostOrderTraversal(node.Right))
-                {
-                    yield return rightNode;
-                }
+                parent = rootNode;
             }
 
-            yield return node.Data;
+            int result = rootNode.Data.CompareTo(item);
+            if (result == 0)
+            {
+                return rootNode;
+            }
+
+            parent = rootNode;
+
+            return result > 0
+                ? FindNode(rootNode.Left, item, ref parent)
+                : FindNode(rootNode.Right, item, ref parent);
         }
 
         private IEnumerable<T> InOrderTraversal(Node<T> node)
@@ -171,23 +189,6 @@ namespace BinaryTree
             }
         }
 
-        private void InsertNode(Node<T> child, Node<T> parent)
-        {
-            if (parent == null)
-            {
-                return;
-            }
-
-            if (parent.Data.CompareTo(child.Data) > 0)
-            {
-                parent.Left = InsertChild(child, parent.Left);
-            }
-            else
-            {
-                parent.Right = InsertChild(child, parent.Right);
-            }
-        }
-
         private Node<T> InsertChild(Node<T> child, Node<T> parent)
         {
             if (parent == null)
@@ -200,22 +201,90 @@ namespace BinaryTree
             return parent;
         }
 
-        private Node<T> FindNode(Node<T> rootNode, T item)
+        private void InsertNode(Node<T> child, Node<T> parent)
         {
-            if (rootNode == null)
+            if (parent == null)
             {
-                return null;
+                return;
             }
 
-            int result = rootNode.Data.CompareTo(item);
-            if (result == 0)
+            if (parent.Data.CompareTo(child.Data) >= 0)
             {
-                return rootNode;
+                parent.Left = InsertChild(child, parent.Left);
+            }
+            else
+            {
+                parent.Right = InsertChild(child, parent.Right);
+            }
+        }
+
+        private bool IsLeaf(Node<T> node)
+        {
+            return node.Right == null && node.Left == null;
+        }
+
+        private IEnumerable<T> PostOrderTraversal(Node<T> node)
+        {
+            if (node == null)
+            {
+                yield break;
             }
 
-            return result > 0
-                ? FindNode(rootNode.Left, item)
-                : FindNode(rootNode.Right, item);
+            if (node.Left != null)
+            {
+                foreach (var leftNode in PostOrderTraversal(node.Left))
+                {
+                    yield return leftNode;
+                }
+            }
+
+            if (node.Right != null)
+            {
+                foreach (var rightNode in PostOrderTraversal(node.Right))
+                {
+                    yield return rightNode;
+                }
+            }
+
+            yield return node.Data;
+        }
+
+        private IEnumerable<T> PreOrderTraversal(Node<T> node)
+        {
+            if (node == null)
+            {
+                yield break;
+            }
+
+            yield return node.Data;
+
+            foreach (T leftNode in PreOrderTraversal(node.Left))
+            {
+                yield return leftNode;
+            }
+
+            foreach (T rightNode in PreOrderTraversal(node.Right))
+            {
+                yield return rightNode;
+            }
+        }
+
+        private void RemoveLeafNode(Node<T> parent, Node<T> leaf)
+        {
+            if (parent.Data.CompareTo(leaf.Data) >= 0)
+            {
+                parent.Left = null;
+            }
+            else
+            {
+                parent.Right = null;
+            }
+        }
+
+        private void RemoveOnlyChild(Node<T> parent, Node<T> foundNode)
+        {
+            parent.Left = foundNode.Left;
+            parent.Right = foundNode.Right;
         }
     }
 }
